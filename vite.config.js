676b8b1a5e3ad02df105a6335e2977a6bcdc4699
ratul0407 <rajaulislamratul12@gmail.com>
2025-05-31 19:49:08 +0600

    using plugin for netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "vite-plugin-netlify";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), netlify()],
 });
