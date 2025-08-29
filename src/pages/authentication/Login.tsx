49851df00bb22d132435afa8bf6326dca6ce9366
ratul0407 <rajaulislamratul12@gmail.com>
2025-08-30 01:39:10 +0600

    handled google login

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -1,4 +1,3 @@
-// import { Link, useNavigate } from "react-router";
 import loginImg from "../../assets/static/login-page-img.jpg";
 import {
   Form,
@@ -17,6 +16,7 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
 import { useLoginMutation } from "@/redux/features/auth.api";
+import config from "@/config";
 const loginSchema = z.object({
   email: z.string(),
   password: z.string().min(8, { error: "Min 8 characters required" }),
@@ -42,7 +42,7 @@ export const Login = () => {
   };
   return (
     <div className="flex min-h-screen items-center">
-      <div className="mx-auto basis-1/2 lg:max-w-lg lg:space-y-12">
+      <div className="mx-auto basis-1/2 lg:max-w-lg lg:space-y-6">
         <div className="flex flex-col gap-4 text-center">
           <Link to="/" className="font-metal text-4xl sm:text-3xl">
             PlantLife
@@ -92,74 +92,26 @@ export const Login = () => {
             <Button>Login</Button>
           </form>
         </Form>
-        {/* <div className="space-y-8">
-          <div className="flex flex-col gap-4 text-center">
-            <Link to="/" className="font-metal text-4xl sm:text-3xl">
-              PlantLife
-            </Link>
-            <p className="font-metal text-3xl text-gray-600">
-              Connect with nature, Connect with your roots
-            </p>
-          </div>
-        </div>
-        <form className="space-y-6" onSubmit={handleSubmit}>
-          <div className="input-container">
-            <label htmlFor="email" className="w-20 font-semibold text-gray-600">
-              Email:{" "}
-            </label>
-            <input
-              type="email"
-              name="email"
-              placeholder="example@gmail.com"
-              className="input"
-              value={values.email}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            {errors.email && touched.email ? (
-              <div className="error-msg">{errors.email}</div>
-            ) : null}
-          </div>
-          <div className="input-container">
-            <label htmlFor="email" className="w-20 font-semibold text-gray-600">
-              Password:
-            </label>
-            <input
-              placeholder="type your password"
-              type={showPass ? "text" : "password"}
-              name="password"
-              className="input"
-              value={values.password}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            <button
-              onClick={() => setShowPass(!showPass)}
-              type="button"
-              className="absolute top-10 right-10 cursor-pointer text-gray-600"
-            >
-              {showPass ? <FiEyeOff /> : <FiEye />}
-            </button>
-            {errors.password && touched.password ? (
-              <div className="error-msg">{errors.password}</div>
-            ) : null}
-          </div>
-          {/* <FormButton isSubmitting={isSubmitting} text={"Login"} /> */}
-        {/* </form>
-        <div>
-          <p className="space-x-2">
-            <span>New to PlantLife?</span>
-            <Link to="/signup" className="font-bold text-green-700 underline">
-              Create an account
-            </Link>
-          </p>
-        </div> */}{" "}
-        {/* <div className="justify-cener flex items-center gap-4">
+
+        <div className="justify-cener flex items-center gap-4">
           <span className="h-[1px] w-full bg-gray-300"></span>
           <p className="text-gray-400">Or</p>
           <span className="h-[1px] w-full bg-gray-300"></span>
-        </div> */}
+        </div>
         {/* <GoogleLogin /> */}
+        <Button
+          onClick={() => window.open(`${config.baseUrl}/auth/google`)}
+          variant="outline"
+          className="w-full"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path
+              d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+              fill="currentColor"
+            />
+          </svg>
+          Login with Google
+        </Button>
       </div>
       <div className="hidden basis-1/2 lg:block">
         <img className="max-h-screen w-full" src={loginImg} />
